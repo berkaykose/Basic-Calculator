ac5bac1415004f1bea095937d85a0309bad9c9a6
Berkay Köse <berkaykose3@hotmail.com>
2019-07-09 15:02:28 +0300

    Basit bir UI kullanarak yazdığım basit bir hesap makinesi ugulaması

--- BasicCalculator/ViewController.swift
+++ BasicCalculator/ViewController.swift
@@ -10,11 +10,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Variables
+    var mark = ""
+    var result = 0
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    //MARK: - IBActions
+    @IBAction func plusButtonAction(_ sender: Any) {
+        mark = "+"
+    }
+    
+    @IBAction func minusButtonAction(_ sender: Any) {
+        mark = "-"
+    }
+    
+    @IBAction func multiplyButtonAction(_ sender: Any) {
+        mark = "*"
+    }
+    
+    @IBAction func divideButtonAction(_ sender: Any) {
+        mark = "/"
+    }
+    
+    @IBAction func calculateButtonAction(_ sender: Any) {
+        if mark == "+" {
+            if let numberOne = Int(firstNumberTextField.text!) , let numberTwo = Int(secondNumberTextField.text!) {
+               result = addition(firstNumber: numberOne, secondNumber: numberTwo)
+            }
+            resultLabel.text = "Sonuç : \(result)"
+        }else if mark == "-" {
+            if let numberOne = Int(firstNumberTextField.text!) , let numberTwo = Int(secondNumberTextField.text!) {
+                result = extraction(firstNumber: numberOne, secondNumber: numberTwo)
+            }
+            resultLabel.text = "Sonuç : \(result)"
+        }else if mark == "*" {
+            if let numberOne = Int(firstNumberTextField.text!) , let numberTwo = Int(secondNumberTextField.text!) {
+                result = multiplication(firstNumber: numberOne, secondNumber: numberTwo)
+            }
+            resultLabel.text = "Sonuç : \(result)"
+        }else if mark == "/" {
+            if let numberOne = Int(firstNumberTextField.text!) , let numberTwo = Int(secondNumberTextField.text!) {
+                result = division(firstNumber: numberOne, secondNumber: numberTwo)
+            }
+            resultLabel.text = "Sonuç : \(result)"
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //MARK: - Functions
+    func addition(firstNumber : Int, secondNumber : Int) -> Int {
+        return firstNumber + secondNumber
+    }
+    
+    func extraction(firstNumber : Int, secondNumber: Int) -> Int {
+        return firstNumber - secondNumber
+    }
+    
+    func multiplication(firstNumber : Int, secondNumber : Int) -> Int {
+        return firstNumber * secondNumber
+    }
+    
+    func division(firstNumber : Int, secondNumber : Int) -> Int {
+        return firstNumber / secondNumber
+    }
+    
 
 }
 
